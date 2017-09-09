a797eade199ae78b823e11332da490192adc2d68
Prashant Koshta <prashantkoshta@gmail.com>
2017-09-09 16:45:52 +0530

    Added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 import { LandingScreen } from './com/ssp/screens/Landing';
@@ -7,14 +7,28 @@ import { HomeScreen } from './com/ssp/screens/Home';
 import { StandardScreen } from './com/ssp/screens/Standard';
 import { SubjectScreen } from './com/ssp/screens/Subject';
 import { LessonScreen } from './com/ssp/screens/Lesson';
-import { ContentScreen } from './com/ssp/screens/Content';
+//import { ContentScreen } from './com/ssp/screens/Content';
 
+/**/
 
 export default App = StackNavigator({
   Landing: { screen: LandingScreen },
   Home: { screen: HomeScreen },
   Standard: { screen: StandardScreen },
   Subject: { screen: SubjectScreen },
-  Lesson: { screen: LessonScreen },
-  Content: { screen: ContentScreen },
+  Lesson: { screen: LessonScreen }
+ // Content: { screen: ContentScreen },
 });
+
+
+/*
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
+
+*/
